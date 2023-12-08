9a783f6d3612b7d03568df69189e2022f24d80df
lamthienphuc1402 <123019955+lamthienphuc1402@users.noreply.github.com>
2023-12-08 14:19:54 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="align-center mt-auto">
+                <h2 className="text-center mx-auto my-auto">ADMIN HOME</h2>
+                <img src="https://img.freepik.com/free-vector/settings-concept-illustration_114360-3754.jpg" className='mt-auto' alt="" />
+            </div>
+        );
+    }
+}
+export default Home;
